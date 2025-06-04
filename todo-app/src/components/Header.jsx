dd0647bbc7cc38c466a69ca52c8ae28c828e7eea
simee56 <pandeysimee7@gmail.com>
2025-06-04 13:57:17 +0530

    changes

--- todo-app/src/components/Header.jsx
+++ todo-app/src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Header = () => {
-    return <h1>Todo List</h1>;
+    return <h1 className="Header">Todo List</h1>;
 };
 
 export default Header;
--- todo-app/src/components/button.jsx
+++ todo-app/src/components/button.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Button = () => {
-    return <button>Add More </button>;
+    return <button className="button">Add More </button>;
 }
  
 export default Button;
