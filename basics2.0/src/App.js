3d27c89d53c43550f122107ad8d5f50ca35ba656
simee56 <pandeysimee7@gmail.com>
2025-06-10 18:09:48 +0530

    Unmounting of UseEffect

--- basics2.0/src/App.js
+++ basics2.0/src/App.js
@@ -1,14 +1,28 @@
-import React , {useEffect} from "react";
+import React , {useEffect, useState } from "react";
 import './App.css';
 
 import MyComponent from './component/useEffecthooks';
 
+
 function App() {
+  const [isVisible, setVisible] = useState(true);
+
   useEffect(() => {
     console.log("App componenet is mounting....");
+
+    //For Unmounting
+    return function() {
+      console.log("Unmounting....")
+    }
+    
   }, [])
   
-  return <MyComponent/>;
-}
+  return (
+    <div>
+     {isVisible ? <MyComponent/> : <></>}
+      <button onClick = {() => setVisible(!isVisible)}>Toggle</button>
+    </div>
+  );
+};
 
 export default App;
