f8dfc807f0509f7c404059a2434ce42acf9f9c19
simee56 <pandeysimee7@gmail.com>
2025-06-10 18:24:05 +0530

    import Timer Component

--- basics2.0/src/App.js
+++ basics2.0/src/App.js
@@ -2,6 +2,8 @@ import React , {useEffect, useState } from "react";
 import './App.css';
 
 import MyComponent from './component/useEffecthooks';
+import Timer from './component/Timer';
+
 
 
 function App() {
@@ -9,18 +11,13 @@ function App() {
 
   useEffect(() => {
     console.log("App componenet is mounting....");
-
-    //For Unmounting
-    return function() {
-      console.log("Unmounting....")
-    }
-    
   }, [])
   
   return (
     <div>
      {isVisible ? <MyComponent/> : <></>}
       <button onClick = {() => setVisible(!isVisible)}>Toggle</button>
+      <Timer/>
     </div>
   );
 };
