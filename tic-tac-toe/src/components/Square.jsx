ae35ce39175aa35e055236976418522ed8ece0bc
simee56 <pandeysimee7@gmail.com>
2025-06-11 13:19:14 +0530

    Use onClick function

--- tic-tac-toe/src/components/Square.jsx
+++ tic-tac-toe/src/components/Square.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Square = (props) => {
     return (
         <div className="Square">
+            onClick={props.onClick}
             <h5>{props.value}</h5>
         </div>
     );
