b5ee274d034e3cb77bcddfb0a35096da7f4e0f50
simee56 <pandeysimee7@gmail.com>
2025-06-13 14:20:54 +0530

    using props

--- api-basics/src/components/postCard.jsx
+++ api-basics/src/components/postCard.jsx
@@ -2,9 +2,10 @@ import React from "react";
 
 const PostCard = (props) => {
     return (
-    <div className="post-card">
-        <h3>{props.Title}</h3>
-        <p>{props.Body}</p>
-    </div>
-    )}
+        <div className="post-card">
+            <h3>{props.Title}</h3>
+            <p>{props.Body}</p>
+        </div>
+    )
+}
 export default PostCard;
