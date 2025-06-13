d2a14ff6765b5367995bbeb790d19f66d8e303c2
simee56 <pandeysimee7@gmail.com>
2025-06-13 14:17:28 +0530

    create a function Postcard

---
+++ api-basics/src/components/postCard.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const PostCard = () => {
+    return (
+    <div className="post-card">
+        <h3>Title</h3>
+        <p>Body</p>
+    </div>
+    )}
+export default PostCard;
