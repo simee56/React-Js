e6c0d9bf12cec0cd716035a9c4c3915a830fbea0
simee56 <pandeysimee7@gmail.com>
2025-06-13 14:10:02 +0530

    use of useEffect and useState

--- api-basics/src/App.js
+++ api-basics/src/App.js
@@ -1,15 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
-import React, {usesState, useEffect} from "react";
-import {getPosts} from './Api';
+import React, { useState, useEffect } from "react";
+import { getPosts } from './Api';
 
 
 function App() {
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    getPosts().then(posts => setData(posts))
+  }, [])
+
   return (
-    <div className="App">
-     
+    (<div className="App">
+      {
+        data ? data.map((e) => <li>{e.title}</li>): <p>Please provide some data.</p>
+      }
     </div>
+    )
   );
 }
 
