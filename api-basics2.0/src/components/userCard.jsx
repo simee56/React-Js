5c8c4ba653d75085d56976506c21770de2204cea
simee56 <pandeysimee7@gmail.com>
2025-06-16 11:12:27 +0530

    use props

---
+++ api-basics2.0/src/components/userCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const UserCard = (props) => {
+    return (
+        <div className="user-card">
+            <h3>{props.data.name.first}</h3>
+            <p>{props.data.phone}</p>
+            <p>{props.data.location.city}, {props.data.location.state}</p>
+        </div>
+    );
+};
+
+export default UserCard;
