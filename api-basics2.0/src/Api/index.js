92761c5afad6bcea9b8fd99387b0b3c95c04bd85
simee56 <pandeysimee7@gmail.com>
2025-06-18 20:17:21 +0530

    changes

--- api-basics2.0/src/Api/index.js
+++ api-basics2.0/src/Api/index.js
@@ -1,7 +1,7 @@
 export const getUsers = async () => {
-    const response = await fetch('https://randomuser.me/api/',{
-         method: "GET" ,
-        }
+    const response = await fetch('https://randomuser.me/api/', {
+        method: "GET",
+    }
     )
     return response.json();
 };
