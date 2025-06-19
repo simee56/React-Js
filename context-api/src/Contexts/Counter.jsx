a320fcc9de53c242d832bf8d4ab8478f95b2d53b
simee56 <pandeysimee7@gmail.com>
2025-06-19 14:05:54 +0530

    use setCount in provider's value

--- context-api/src/Contexts/Counter.jsx
+++ context-api/src/Contexts/Counter.jsx
@@ -5,7 +5,7 @@ export const counterContext = createContext(null);
 export const CounterProvider = (props) => {
     const [count, setCount] = useState(0);
     return (
-        <counterContext.Provider value={{ count }} >
+        <counterContext.Provider value={{ count, setCount }} >
             {props.children}
         </counterContext.Provider>
     );
