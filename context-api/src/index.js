873cd0f256ccb1f69b32e0e72fe00d2d5a01c6d4
simee56 <pandeysimee7@gmail.com>
2025-06-19 13:49:05 +0530

    changes

--- context-api/src/index.js
+++ context-api/src/index.js
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import { counterProvider } from './Contexts/Counter';
+import { CounterProvider } from './Contexts/Counter';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <counterProvider />
+    <CounterProvider />
     <App />
-    <counterProvider />
+    <CounterProvider />
   </React.StrictMode>
 );
 
