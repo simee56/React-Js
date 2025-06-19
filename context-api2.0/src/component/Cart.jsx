d13f267f2bed97c1f2f96686b8186a976bdae6d5
simee56 <pandeysimee7@gmail.com>
2025-06-19 18:22:00 +0530

    changes

--- context-api2.0/src/component/Cart.jsx
+++ context-api2.0/src/component/Cart.jsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { cartContext } from "../context/cart";
 
 const Cart = () => {
+    const cart = useContext(cartContext)
     return (
         <div>
-            <li>
-                MacBook - $100000
-            </li>
-
+            <h1>Cart</h1>
+            {
+                cart && cart.items.map((item) => (
+                    <li>
+                        {item.name} -{item.price}
+                    </li>
+                )
+                )
+            }
             <h4>Total Amount</h4>
         </div>
     );
