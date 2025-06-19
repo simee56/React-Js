485b4a872dbed07fe55142ecf434951620d0aef4
simee56 <pandeysimee7@gmail.com>
2025-06-19 18:00:27 +0530

    Create a Cart component

---
+++ context-api2.0/src/component/Cart.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Cart = () => {
+    return (
+        <div>
+            <li>
+                MacBook - $100000
+            </li>
+
+            <h4>Total Amount</h4>
+        </div>
+    );
+};
+
+export default Cart;
