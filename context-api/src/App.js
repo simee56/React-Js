d1514e3ecfeb84a55eb29e1fd2c1495f9e2cd362
simee56 <pandeysimee7@gmail.com>
2025-06-19 14:03:15 +0530

    import useContext and counterContext

--- context-api/src/App.js
+++ context-api/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Counter from './component/Counter';
+import { useContext } from 'react';
+import { counterContext } from './Contexts/Counter';
 
 function App() {
+  const counterState = useContext(counterContext);
+
   return (
     <div className="App">
-      <h1>Count is 0</h1>
+      <h1>Count is {counterState.count} </h1>
       <Counter />
       <Counter />
       <Counter />
