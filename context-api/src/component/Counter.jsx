5c3abde4a53c5114813b3f34b90cc24aadfc96e6
simee56 <pandeysimee7@gmail.com>
2025-06-19 14:11:22 +0530

    use counterState for increment

--- context-api/src/component/Counter.jsx
+++ context-api/src/component/Counter.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import { useContext } from "react";
+import { counterContext } from "../Contexts/Counter";
 
 const Counter = () => {
+    const counterState = useContext(counterContext);
     return (
         <div>
-            <button>Increment</button>
+            <button onClick={() => counterState.setCount(counterState.count +1)}>Increment</button>
             <button>Decrement</button>
         </div>
     )
