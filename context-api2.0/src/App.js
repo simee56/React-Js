15273ea0722c7acaa1c9a0a40b3b65f59cf861b0
simee56 <pandeysimee7@gmail.com>
2025-06-19 18:01:09 +0530

    import Cart component

--- context-api2.0/src/App.js
+++ context-api2.0/src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Item from './component/Item';
+import Cart from './component/Cart';
 
 function App() {
   return (
     <div className="App">
-      <Item name="MAcBook" price="{100000}" />
-      <Item name="PenDrive" price="{1000}" />
-      <Item name="Pen" price="{50}" />
-      <Item name="Tops" price="{300}" />
-
+      <Item name="MAcBook" price={100000} />
+      <Item name="PenDrive" price={1000} />
+      <Item name="Pen" price={50} />
+      <Item name="Tops" price={300} />
+      <Cart />
     </div>
   );
 }
