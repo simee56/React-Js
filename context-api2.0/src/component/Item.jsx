219db8a72a1b88cb178d38808643557c2801f28b
simee56 <pandeysimee7@gmail.com>
2025-06-19 18:29:04 +0530

    use cartContext in button

--- context-api2.0/src/component/Item.jsx
+++ context-api2.0/src/component/Item.jsx
@@ -1,11 +1,15 @@
-import React from "react";
+import { React, useContext } from "react";
+import { cartContext } from "../context/cart";
 
 const Item = (props) => {
+    const cart = useContext(cartContext);
+    console.log("cart:", cart);
+
     return (
         <div>
             <h3>{props.name} </h3>
             <h3>Price : {props.price} </h3>
-            <button>Add to cart</button>
+            <button onClick={() => cart.setItems([...cart.items, { name: props.name, price: props.price }])}>Add to cart</button>
         </div>
     );
 };
