3c150e56c6cffa28fda8e29c23bc64ae2f3f1685
simee56 <pandeysimee7@gmail.com>
2025-06-19 18:06:35 +0530

    create a CartContext

---
+++ context-api2.0/src/context/cart.jsx
@@ -0,0 +1,13 @@
+import { createContext, useState } from "react";
+
+const cartContext = createContext(null);
+
+export const CartProvider = (props) => {
+    const [items, setItems] = useState([]);
+
+    return (
+        <cartContext.Provider value={{ items, setItems }} >
+            {props.children}
+        </cartContext.Provider>
+    );
+};
