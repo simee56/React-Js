f4a346f7cb826ec619ea471cbec33aca9152cbc2
simee56 <pandeysimee7@gmail.com>
2025-06-20 22:05:04 +0530

    create a card component

---
+++ weather-app/src/components/card.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Card = () => {
+    return (
+        <div className="Card">
+            <img src="mypic.png" />
+            <h3>32 C</h3>
+            <h5>London,City of London, Greater London </h5>
+        </div>
+    )
+};
+
+export default Card;
