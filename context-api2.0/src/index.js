24fe404b7b145906bd002cb1f43ef7f3d0ef8b5b
simee56 <pandeysimee7@gmail.com>
2025-06-21 21:56:01 +0530

    chnages

--- context-api2.0/src/index.js
+++ context-api2.0/src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <CartProvider>
-    <App />
+      <App />
     </CartProvider>
   </React.StrictMode>
 );
