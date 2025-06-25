bd92c1155131405506375546f06d2e6c8a21115d
simee56 <pandeysimee7@gmail.com>
2025-06-25 11:45:17 +0530

    import useweather context

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,14 +1,17 @@
+import { use } from 'react';
 import './App.css';
 import Button from './components/button';
 import Card from './components/card';
 import Search from './components/search';
+import { useWeather } from './contexts/weather';
 
 function App() {
+  const weather = useWeather();
   return (
     <div className="App">
       <h1>Weather Forecast</h1>
       <Search />
-      <Button value="Search" />
+      <Button onclick={weather.fetchData} value="Search" />
       <Card />
       <Button value="Refresh" />
     </div>
