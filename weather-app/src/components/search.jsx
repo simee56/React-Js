b0f632f43d40c599bbe2f87c9fc753c8c826bf9f
simee56 <pandeysimee7@gmail.com>
2025-06-27 13:21:06 +0530

    chnages

--- weather-app/src/components/search.jsx
+++ weather-app/src/components/search.jsx
@@ -2,11 +2,9 @@ import React from "react";
 
 const Search = (props) => {
     return (
-        <div className="Search">
-            <Input placeholder={props.placeholder}
-                value={props.value}
-                onChange={props.onChange} />
-        </div>
+        <input claaName="Search" placeholder={props.placeholder}
+            value={props.value}
+            onChange={props.onChange} />
     )
 };
 
