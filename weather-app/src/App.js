4639426268c76ada276fb5baffde795a41cf2f4e
simee56 <pandeysimee7@gmail.com>
2025-06-27 13:20:42 +0530

    import components

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,11 +1,18 @@
-
 import './App.css';
+import Button from './components/button';
+import Search from './components/search';
+import Card from './components/card';
 
 
 function App() {
   return (
     <div className="App">
       <h1>Weather Forecast</h1>
+      <Search />
+      <Button value="Search" />
+      <Card />
+      <Button value="Refresh" />
+
 
     </div>
   );
