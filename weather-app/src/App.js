c3349af7ab1732caeb4636ae2ba59231902801ca
simee56 <pandeysimee7@gmail.com>
2025-06-28 12:03:18 +0530

    use useEffect for current location

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -2,14 +2,21 @@ import './App.css';
 import Button from './components/button';
 import Search from './components/search';
 import Card from './components/card';
-
+import { useWeather } from './context/weather';
+import { useEffect } from 'react';
 
 function App() {
+  const weather = useWeather();
+
+  useEffect(() => {
+    weather.fetchCurrentUserLocationData();
+  },[]);
+  
   return (
     <div className="App">
       <h1>Weather Forecast</h1>
       <Search />
-      <Button value="Search" />
+      <Button onClick={weather.fetchData} value="Search" />
       <Card />
       <Button value="Refresh" />
 
