69d28fe2cae2a9904f848b0e28fff2cc3b590868
simee56 <pandeysimee7@gmail.com>
2025-06-28 11:40:52 +0530

    create a useWeather hook

--- weather-app/src/components/card.jsx
+++ weather-app/src/components/card.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useWeather } from "../context/weather";
 
 const Card = () => {
+    const weather = useWeather();
+
     return(
         <div className="card">
-            <img src="mypic.png" />
-            <h2>32 C</h2>
-            <h5>London, City of London, Greater London</h5>
+            <img src={weather?.data?.current?.condition?.icon} />
+            <h2>{weather.data?.current?.temp_c}. C</h2>
+            <h5>{weather.data?.location.name} {weather.data?.location.region} {weather.data?.location.country} </h5>
         </div>
     )
 };
