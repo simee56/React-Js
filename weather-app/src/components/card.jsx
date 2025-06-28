63129f7e9ee2c6c4c052a4f2d2f47ecac43dc75d
simee56 <pandeysimee7@gmail.com>
2025-06-28 12:01:47 +0530

    changes

--- weather-app/src/components/card.jsx
+++ weather-app/src/components/card.jsx
@@ -8,7 +8,7 @@ const Card = () => {
         <div className="card">
             <img src={weather?.data?.current?.condition?.icon} />
             <h2>{weather.data?.current?.temp_c}. C</h2>
-            <h5>{weather.data?.location.name} {weather.data?.location.region} {weather.data?.location.country} </h5>
+            <h5>{weather.data?.location.name} {weather.data?.location.region}{" "} {weather.data?.location.country} </h5>
         </div>
     )
 };
