c39a9aebe5b66839dccaa60b6cd01bcafbd9b71f
simee56 <pandeysimee7@gmail.com>
2025-07-06 18:09:43 +0530

    create a card component

---
+++ meme-generator/src/components/card.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+const MemeCard = (props) => {
+    return (
+        <Card style={{ width: '10rem', margin: '25px' }}>
+            <Card.Img variant="top" src={props.img} />
+            <Card.Body>
+                <Card.Title>{props.title}</Card.Title>
+                <Button variant="primary">Edit</Button>
+            </Card.Body>
+        </Card>
+    )
+};
+
+export default MemeCard;
