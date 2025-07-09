beebce037b0e6f74532d2aa554383c35cc7a7108
simee56 <pandeysimee7@gmail.com>
2025-07-09 11:56:28 +0530

    import EditPage

--- meme-generator/src/App.js
+++ meme-generator/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import {Route, Routes} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import Homepage from './pages/home';
+import EditPage from './pages/edit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Meme Generator</h1>
+      <Routes>
+        <Route path="/" element={<Homepage/>} />
+        <Route path="/edit/:imgUrl" element={<EditPage/>} />
+      </Routes>
     </div>
   );
 }
