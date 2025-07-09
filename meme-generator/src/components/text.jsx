c76fb13f115581c22e73579ecc0b398223061688
simee56 <pandeysimee7@gmail.com>
2025-07-09 15:26:17 +0530

    give styles

--- meme-generator/src/components/text.jsx
+++ meme-generator/src/components/text.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import Draggable from "react-draggable";
 
 const Text = () => {
-    return <h1>Im a text</h1>
+    const [editMode, setEditMode] = useState(false);
+    const [value, setValue] = useState("Double click to Edit");
+
+
+    return (
+        <Draggable>
+            <div
+                style={{ position: "absolute", cursor: "move" }}>
+                {
+                    editMode ?
+                        (
+                            <textarea
+                                type="text"
+                                autoFocus
+                                style={{
+                                    fontSize: "24px",
+                                    padding: "8px",
+                                    minWidth: "250px",
+                                    maxWidth: "600px",
+                                    minHeight : "400px",
+                                    resize : "both",
+                                    overflow : "auto",
+                                }}
+                                onDoubleClick={(e) => setEditMode(false)}
+                                value={value}
+                                onChange={(e) => setValue(e.target.value)} />
+                        ) : (
+
+                            <h1 onDoubleClick={(e) => setEditMode(true)}
+                                style={{
+                                    fontSize: "24px",
+                                    whiteSpace: "pre-wrap",
+                                    wordBreak : "break-word",
+                                    color :"white",
+                                    textShadow: "2px 2px 4px #000",
+                                    padding : "5px",
+                                    maxWidth :"600px",
+                                }}
+                            >
+                                {value}</h1>
+                        )}
+            </div>
+        </Draggable >
+    )
 }
 
 export default Text;
