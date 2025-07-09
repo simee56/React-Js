314acff04479d73ed17be037a76e8a5f42f86ab7
simee56 <pandeysimee7@gmail.com>
2025-07-09 12:27:11 +0530

    use Params

--- meme-generator/src/pages/edit.jsx
+++ meme-generator/src/pages/edit.jsx
@@ -1,9 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
+import { useSearchParams } from "react-router-dom";
+import Text from "../components/text";
 
 const EditPage = () => {
+    const [params] = useSearchParams();
+    const[count, setCount] = useState(0);
+
+    const addText = () => {
+        setCount(count+1)
+    };
+
     return (
         <div>
-            <h1>Edit page</h1>
+            <div>
+                <img src={params.get("url")} width="350px" />
+                {
+                    Array(count).fill(0).map(e => <Text/>)
+                }
+            </div>
+            <button onClick={addText}  >Add Text</button>
         </div>
     );
 };
