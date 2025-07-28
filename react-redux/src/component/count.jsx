65e7b071108ba071b88a85d2522f46c86747e5f8
simee56 <pandeysimee7@gmail.com>
2025-07-28 15:10:42 +0530

    changes

--- react-redux/src/component/count.jsx
+++ react-redux/src/component/count.jsx
@@ -1,6 +1,13 @@
-const Count = () => {
-    return <h1>Count is 0</h1>
+import React from "react";
+import { useSelector } from "react-redux";
 
+const Count = () => {
+    const count = useSelector((state) => state);
+    return (
+        <div>
+            <h1>{count}</h1>
+        </div>
+    )
 };
 
 export default Count;
