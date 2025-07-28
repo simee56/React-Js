92d138e061d4d1575a04deea28f1cd6a28373c22
simee56 <pandeysimee7@gmail.com>
2025-07-28 15:12:52 +0530

    changes

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+import Count from './component/count';
+import { useDispatch } from 'react-redux';
 
 function App() {
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={(e) => dispatch({ type: 'INCREMENT' })}>Increment</button>
+      <Count />
+      <button onClick={(e) => dispatch({ type: 'DECREMENT' })}>Decrement</button>
     </div>
   );
 }
