89fd21e2d243c402214ff67996b5b9e5eb820b35
simee56 <pandeysimee7@gmail.com>
2025-09-06 18:11:21 +0530

    add a case if action is rejected

--- redux-api/src/redux/slice/todo.js
+++ redux-api/src/redux/slice/todo.js
@@ -1,7 +1,34 @@
-import {createSlice} from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-const todoSlice =  createSlice({
-    name : todo,
+//Dispatching action
+export const fetchTodos = createAsyncThunk('fetchTodos', async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+    return response.json();
+});
+
+const todoSlice = createSlice({
+    name: 'todo',
+    initialState: {
+        isLoading: false,
+        data: null,
+        isError: false
+    },
+
+    extraReducers: (builder) => {
+        builder.addCase(fetchTodos.pending, (state, action) => {
+            state.isLoading = true;
+        });
+
+        builder.addCase(fetchTodos.fulfilled, (state, action) => {
+            state.isLoading = false;
+                state.data = action.payload
+        });
+
+        builder.addCase(fetchTodos.rejected, (state, action) => {
+            console.log('Error', action.payload);
+            state.isError = true
+        });
+    }
 }
 )
 export default todoSlice.reducer;
