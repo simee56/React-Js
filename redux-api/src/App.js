efbf1d5030ceeba5361c7e3435a3d815d93a2f21
simee56 <pandeysimee7@gmail.com>
2025-09-06 18:12:02 +0530

    using useDispatch and useSelector

--- redux-api/src/App.js
+++ redux-api/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTodos } from './redux/slice/todo';
 
 function App() {
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+
+  if (state.todo.isLoading) {
+    return <h1>Loading...</h1>
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={(e) => dispatch(fetchTodos())}>Fetch Data</button>
+      {state.todo.data && state.todo.data.map((e) => <li>{e.title}</li>)};
     </div>
   );
 }
