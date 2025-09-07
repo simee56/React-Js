dc56a4efe1b282616fccdc7e5fa7a7f82bc047ef
simee56 <pandeysimee7@gmail.com>
2025-09-07 20:51:29 +0530

    changes

--- firebase_auth/src/App.js
+++ firebase_auth/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import { app } from './firebase';
 
 function App() {
+  const auth = getAuth(app);
+
+  const signupUser = () => {
+    createUserWithEmailAndPassword(
+      auth,
+      "pandeysimee7@gmail.com",
+      "abpusa"
+    )
+      .then((value) => console.log(value))
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={signupUser}>Create User</button>
     </div>
   );
 }
