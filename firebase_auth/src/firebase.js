9f5772c4aafc65d2109e8871ded0ccf9255061eb
simee56 <pandeysimee7@gmail.com>
2025-09-07 20:51:52 +0530

    changes

---
+++ firebase_auth/src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjHFXSotmg-LcvIJadntnNAJstrYOWGe4",
+  authDomain: "firstproject-ee77a.firebaseapp.com",
+  projectId: "firstproject-ee77a",
+  storageBucket: "firstproject-ee77a.firebasestorage.app",
+  messagingSenderId: "194258121649",
+  appId: "1:194258121649:web:2941bb9d4cd879dd7fc390",
+  databaseUrl : "https://firstproject-ee77a-default-rtdb.firebaseio.com/"
+};
+
+export const app = initializeApp(firebaseConfig);
+
+ 
