b0602cd37b84f25ef4611fd2e50da85d381babbd
simee56 <pandeysimee7@gmail.com>
2025-09-07 15:33:59 +0530

    changes

--- firebase/src/firebase.js
+++ firebase/src/firebase.js
@@ -6,7 +6,10 @@ const firebaseConfig = {
   projectId: "firstproject-ee77a",
   storageBucket: "firstproject-ee77a.firebasestorage.app",
   messagingSenderId: "194258121649",
-  appId: "1:194258121649:web:2941bb9d4cd879dd7fc390"
+  appId: "1:194258121649:web:2941bb9d4cd879dd7fc390",
+  databaseUrl : "https://firstproject-ee77a-default-rtdb.firebaseio.com/"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
+
+ 
