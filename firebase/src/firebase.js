5b624a384e443ce22ebf9ecbfb0f7d355678a856
simee56 <pandeysimee7@gmail.com>
2025-09-07 15:15:07 +0530

    initialize and config firebase app

---
+++ firebase/src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjHFXSotmg-LcvIJadntnNAJstrYOWGe4",
+  authDomain: "firstproject-ee77a.firebaseapp.com",
+  projectId: "firstproject-ee77a",
+  storageBucket: "firstproject-ee77a.firebasestorage.app",
+  messagingSenderId: "194258121649",
+  appId: "1:194258121649:web:2941bb9d4cd879dd7fc390"
+};
+
+const app = initializeApp(firebaseConfig);
