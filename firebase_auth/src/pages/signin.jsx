12a8634cd8fbcb8af032a021ddb87834e4665226
simee56 <pandeysimee7@gmail.com>
2025-09-08 10:42:28 +0530

    signin using email and password

--- firebase_auth/src/pages/signin.jsx
+++ firebase_auth/src/pages/signin.jsx
@@ -2,15 +2,34 @@ import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { app } from "../firebase";
 import { React, useState } from "react";
 
+const auth = getAuth(app);
 
-const SignInPage = ()=> {
+const SignInPage = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const Signinuser = () => {
+        signInWithEmailAndPassword(auth, email, password)
+            .then((value) => console.log("signIn success"))
+            .catch((err) => console.log(err));
+    }
     return (
         <div className="signin-page">
             <h1>Signin Page</h1>
             <label>Enter your email</label>
-            <input type="email" placeholder="Enter your email" />
-             <label>Enter your password</label>
-            <input type="password" placeholder="Enter your password" />
+            <input
+                onChange={e => setEmail(e.target.value)}
+                value={email}
+                type="email"
+                placeholder="Enter your email"
+            />
+            <label>Enter your password</label>
+            <input
+                onChange={e => setPassword(e.target.value)}
+                value={password}
+                type="password"
+                placeholder="Enter your password"
+            />
             <button>Singin</button>
         </div>
     )
