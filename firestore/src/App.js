408ab4a2c06e4cb0484512bb51c3c6da74890943
simee56 <pandeysimee7@gmail.com>
2025-09-12 22:02:55 +0530

    Read a document

--- firestore/src/App.js
+++ firestore/src/App.js
@@ -1,4 +1,4 @@
-import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getFirestore, collection, addDoc, doc, getDoc } from "firebase/firestore";
 import { app } from "./firebase"
 import './App.css'
 
@@ -6,18 +6,39 @@ const fireStore = getFirestore(app);
 
 function App() {
 
-  const writeData = async() => {
+  const writeData = async () => {
     const result = await addDoc(collection(fireStore, "Cities"), {
-      name :"Delhi",
-      pincode : 123
+      name: "Delhi",
+      pincode: 123
     });
     console.log("Result", result)
+  };
+
+  // TO CREATE A SUB-COLLECTION.
+  const writeSubData = async () => {
+    const reusltOfSubData = await addDoc(collection(fireStore, "Cities/BgvFbSuzl5l1ahyzCadr/places"), {
+      name: "Noida2",
+      pincode: 56
+    });
+    console.log("Result", reusltOfSubData)
+  };
+
+  //TO READ A DOCUMENT 
+  const getDocument = async () => {
+    const ref = doc(fireStore, "Cities", "BgvFbSuzl5l1ahyzCadr");
+    const snap = await getDoc(ref);
+
+    console.log(snap.data());
   };
 
   return (
     <div className="App">
       <h1>Firebase Firestore</h1>
       <button onClick={writeData}>Put Data</button>
+      <button onClick={writeSubData}>Put SubData</button>
+      <button onClick={getDocument}>Get Document</button>
+
+
     </div>
   );
 }
