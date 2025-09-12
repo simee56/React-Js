d91586e8831defd3b8adf002b4de49b83288c24e
simee56 <pandeysimee7@gmail.com>
2025-09-12 21:31:32 +0530

    Create a collection

--- firestore/src/App.js
+++ firestore/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { app } from "./firebase"
+import './App.css'
+
+const fireStore = getFirestore(app);
 
 function App() {
+
+  const writeData = async() => {
+    const result = await addDoc(collection(fireStore, "Cities"), {
+      name :"Delhi",
+      pincode : 123
+    });
+    console.log("Result", result)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Firebase Firestore</h1>
+      <button onClick={writeData}>Put Data</button>
     </div>
   );
 }
