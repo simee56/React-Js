ff124e65233a696088ecc8f2905acb1861859006
simee56 <pandeysimee7@gmail.com>
2025-09-14 20:00:01 +0530

    changes

--- bookify/src/index.js
+++ bookify/src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { FirebaseProvider } from "./context/firebase";
-import {BrowserRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-    <FirebaseProvider>
-      <App />
-    </FirebaseProvider>
+      <FirebaseProvider>
+        <App />
+      </FirebaseProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
