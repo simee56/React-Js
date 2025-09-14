20f86cc3f8ace07630d54a96636440d4afdd5d23
simee56 <pandeysimee7@gmail.com>
2025-09-14 19:56:05 +0530

    import Routes

--- bookify/src/context/firebase.jsx
+++ bookify/src/context/firebase.jsx
@@ -1,9 +1,22 @@
-import {createContext, useContext} from "react";
+import { createContext, useContext } from "react";
+import { initializeApp } from "firebase/app";
 
 const firebaseContext = createContext(null);
 
-export const useFirebase = useContext(firebaseContext);
+const firebaseConfig = {
+    apiKey: "AIzaSyDjHFXSotmg-LcvIJadntnNAJstrYOWGe4",
+    authDomain: "firstproject-ee77a.firebaseapp.com",
+    projectId: "firstproject-ee77a",
+    storageBucket: "firstproject-ee77a.firebasestorage.app",
+    messagingSenderId: "194258121649",
+    appId: "1:194258121649:web:2941bb9d4cd879dd7fc390",
+    databaseUrl: "https://firstproject-ee77a-default-rtdb.firebaseio.com/"
+};
 
-export const FirebaseProvider =(props) => {
+export const useFirebase = () => useContext(firebaseContext);
+
+export const app = initializeApp(firebaseConfig);
+
+export const FirebaseProvider = (props) => {
     return <firebaseContext.Provider>{props.children}</firebaseContext.Provider>
 }
