d5c048f6f99c432d0a7489b8e439734c8a483583
simee56 <pandeysimee7@gmail.com>
2025-09-14 22:37:32 +0530

    use useState to update emaila nd password

--- bookify/src/pages/register.jsx
+++ bookify/src/pages/register.jsx
@@ -1,27 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-
+import { useFirebase } from "../context/firebase"
 
 const RegisterPage = () => {
+
+    const firebase = useFirebase();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    console.log(firebase);
+
     return <div className="container">
         <Form>
             <Form.Group className="mb-3" controlId="formBasicEmail">
                 <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control
+                    onChange={e => setEmail(e.target.value)}
+                    value={email}
+                    type="email"
+                    placeholder="Enter email"
+                />
                 <Form.Text className="text-muted">
                 </Form.Text>
             </Form.Group>
 
             <Form.Group className="mb-3" controlId="formBasicPassword">
                 <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
+                <Form.Control
+                    onChange={e => setPassword(e.target.value)}
+                    value={password}
+                    type="password"
+                    placeholder="Password"
+                />
             </Form.Group>
             <Button variant="primary" type="submit">
-                Submit
+                Create Account
             </Button>
         </Form>
     </div>
