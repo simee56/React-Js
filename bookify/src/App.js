3ce72bc2bf0c5fc6233fe0fb70ea119f2bb21b1a
simee56 <pandeysimee7@gmail.com>
2025-09-14 20:00:37 +0530

    changes

--- bookify/src/App.js
+++ bookify/src/App.js
@@ -1,10 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <Routes>
+      <Route path="/" element={<h1>Home page</h1>}></Route>
+      <Route path="/" element={<h1>Login page</h1>}></Route>
+
+    </Routes>
   );
 }
 
