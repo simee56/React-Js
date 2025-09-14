eb54e4a7be2a5581c0696bd5d6bce40c653d5022
simee56 <pandeysimee7@gmail.com>
2025-09-14 19:47:06 +0530

    import BrowserRouter

--- bookify/src/index.js
+++ bookify/src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {FirebaseProvider} from "./context/firebase";
+import { FirebaseProvider } from "./context/firebase";
+import {BrowserRouter} from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <FirebaseProvider>
-    <App />
+      <App />
     </FirebaseProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
