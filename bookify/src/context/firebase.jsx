fe6526f4c48f968db11ac1973d877043a420d7db
simee56 <pandeysimee7@gmail.com>
2025-09-14 22:59:51 +0530

    create a function for login

--- bookify/src/context/firebase.jsx
+++ bookify/src/context/firebase.jsx
@@ -1,18 +1,22 @@
 import { createContext, useContext } from "react";
 import { initializeApp } from "firebase/app";
-import { getAuth, createUserWithEmailAndPassword, EmailAuthCredential } from "firebase/auth";
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
+}
+    from "firebase/auth";
 
 
 const firebaseContext = createContext(null);
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDjHFXSotmg-LcvIJadntnNAJstrYOWGe4",
-    authDomain: "firstproject-ee77a.firebaseapp.com",
-    projectId: "firstproject-ee77a",
-    storageBucket: "firstproject-ee77a.firebasestorage.app",
-    messagingSenderId: "194258121649",
-    appId: "1:194258121649:web:2941bb9d4cd879dd7fc390",
-    databaseUrl: "https://firstproject-ee77a-default-rtdb.firebaseio.com/"
+    apiKey: "AIzaSyBK1NVPlytDaaKu4rQ94Jcm4cn2SBnbjQw",
+    authDomain: "bookify-92867.firebaseapp.com",
+    projectId: "bookify-92867",
+    storageBucket: "bookify-92867.firebasestorage.app",
+    messagingSenderId: "535255944444",
+    appId: "1:535255944444:web:1b83a3fb15ff6e2fa99b79"
 };
 
 export const useFirebase = () => useContext(firebaseContext)
@@ -22,12 +26,19 @@ const firebaseAuth = getAuth(firebaseApp);
 
 
 export const FirebaseProvider = (props) => {
-    const signupUSerWithEmailAndPassword = (email, password) => {
+    const signupUserWithEmailAndPassword = (email, password) => {
         createUserWithEmailAndPassword(firebaseAuth, email, password)
     };
 
+    const signinUserWithEmailAndPassword = (email, password) => {
+        signInWithEmailAndPassword(firebaseAuth, email, password)
+    };
+
     return <firebaseContext.Provider
-        value={{ signupUSerWithEmailAndPassword }}>
+        value={{
+            signupUserWithEmailAndPassword,
+            signInWithEmailAndPassword
+        }}>
         {props.children}
     </firebaseContext.Provider>
 }
