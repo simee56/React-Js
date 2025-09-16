79e0459ffae978d733c534f976d284234973d4dc
simee56 <pandeysimee7@gmail.com>
2025-09-16 20:31:03 +0530

    add Add listing

--- bookify/src/components/navbar.jsx
+++ bookify/src/components/navbar.jsx
@@ -10,8 +10,9 @@ const MyNavbar = () => {
                 <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#features">Features</Nav.Link>
-                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="#home">Add Listing</Nav.Link>
+
+
                 </Nav>
             </Container>
         </Navbar>
